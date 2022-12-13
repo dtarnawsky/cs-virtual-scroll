7db72f66d7cd1ceafdb930a98eaf1a1c288159e4
Damian Tarnawsky <damian@ionic.io>
2022-12-13 07:51:29 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.virtual.scroll',
+  appName: 'cs-virtual-scroll',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
